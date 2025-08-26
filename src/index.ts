c0c8812335b732276d3a7cdb4b2f85b0ceff1195
ken <yash20004x17@gmail.com>
2025-08-26 07:45:57 +0000

    completed the Video 1 on websockets

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,9 @@ const wss = new WebSocketServer({ port: 8080 });
 
 wss.on("connection", function (socket) {
   console.log("User Connected !!");
-  setInterval(() => {
-    socket.send("Price of SOL is " + Math.random());
-  }, 1000);
-
-    socket.on("message", (e) => {
-        console.log(e.toString());
-        
-    })
+  socket.on("message", (e) => {
+    if (e.toString()) {
+      socket.send(e.toString());
+    }
+  });
 });
